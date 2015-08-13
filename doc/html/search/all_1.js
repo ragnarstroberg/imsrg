e91981f476f12ca17f01af87042d7cd3a303718d
Ragnar-Linux <ragnarstroberg@gmail.com>
2015-08-12 17:19:18 -0700

    Fixed the Operator& bug.

--- doc/html/search/all_1.js
+++ doc/html/search/all_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['bch_5fproduct',['BCH_Product',['../classOperator.html#a6a91cd02db723ee28b7532e6af029033',1,'Operator']]],
-  ['bch_5ftransform',['BCH_Transform',['../classOperator.html#adc5537604f6b71121163370aca79dfef',1,'Operator']]],
+  ['bch_5ftransform',['BCH_Transform',['../classOperator.html#a9a2d6b13d851f2655f3ef88033a24c5b',1,'Operator']]],
   ['benchmarking',['Benchmarking',['../benchmarking.html',1,'']]],
   ['buildmonopolev',['BuildMonopoleV',['../classHartreeFock.html#a3d6bac9b4403e4bc599a89ad0c9b6056',1,'HartreeFock']]],
   ['buildmonopolev3',['BuildMonopoleV3',['../classHartreeFock.html#a1c146af25a09f427cc626d877ec6e518',1,'HartreeFock']]]
