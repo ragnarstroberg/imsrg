2d55aa4528600c1fc18c35ee3983bb135ef613cd
Ragnar Stroberg <ragnarstroberg@gmail.com>
2016-08-18 11:59:51 -0700

    insignificant changes to Makefile

--- work/scripts/runUniversal.sh
+++ work/scripts/runUniversal.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-exe=../compiled/Universal
+exe=imsrg++
+#exe=../compiled/Universal
 #exe="valgrind --track-origins=yes ../compiled/Universal"
+#exe="valgrind --track-origins=yes imsrg++"
 
 #vnn=/itch/exch/BlockGen/me2j/chi2b_srg0800_eMax12_lMax10_hwHO020.me2j.gz
 #v3n=/itch/exch/me3j/chi2b3b400cD-02cE0098_srg0800ho40C_eMax14_EMax14_hwHO020.me3j.gz
@@ -25,7 +27,9 @@ for ((A=18;A<=18;A++)); do
 
 hw=20
 #valence_space=O$A
-valence_space=sd-shell
+#valence_space=sd-shell
+#valence_space=O16,p0d5,n0d5,p0d3,n0d3,p1s1,n1s1
+valence_space="customsd"
 #valence_space=Ca40
 #valence_space=Si34
 #valence_space=psd-shell
@@ -34,7 +38,8 @@ valence_space=sd-shell
 #reference=Si28
 reference=O$A
 #A=18
-smax=200
+#smax=200
+smax=2
 emax=4
 e3max=12
 #method=HF
@@ -45,15 +50,19 @@ method=magnus
 omega_norm_max=0.25
 #flowfile=output/BCH_SingleRef_${}_e${emax}.dat
 file3='file3e1max=12 file3e2max=28 file3e3max=12'
+Operators=
 #Operators=Rp2,Rm2,HCM,HCM_28
 #Operators=Rp2,rho0.0,rho0.2,rho0.5,rho1.0,rho1.2,rho1.5,rho2.0,rho2.5,rho3.0,rho3.5,rho4.5,rho6.0
 #Operators=E2
 #Operators=protonFBC1,protonFBC2,protonFBC3,protonFBC4,protonFBC5,protonFBC6,protonFBC7,protonFBC8,protonFBC9
-Operators=
 scratch=SCRATCH
+other_arguments="SPWF=p0s1,p0p3"
+other_arguments=${other_arguments}" custom_valence_space=O16,p0d5,n0d5,p0d3,n0d3,p1s1,n1s1"
 #scratch=
 
-$exe 2bme=${vnn} 3bme=${v3n} emax=${emax} e3max=${e3max} method=${method} valence_space=${valence_space} hw=${hw} smax=${smax} ${file3} omega_norm_max=${omega_norm_max} reference=${reference} Operators=${Operators} scratch=${scratch} A=${A} use_brueckner_bch=true
+OMP_NUM_THREADS=1
+
+$exe 2bme=${vnn} 3bme=${v3n} emax=${emax} e3max=${e3max} method=${method} valence_space=${valence_space} hw=${hw} smax=${smax} ${file3} omega_norm_max=${omega_norm_max} reference=${reference} Operators=${Operators} scratch=${scratch} A=${A} use_brueckner_bch=false $other_arguments
 
 done
 
