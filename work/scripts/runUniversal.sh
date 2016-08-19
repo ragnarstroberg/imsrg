911003402a8095dcaab5c551093596522a3b889b
ragnar-MacBookAir <ragnarstroberg@gmail.com>
2016-08-18 22:50:16 -0700

    messed with runUniversal.sh

--- work/scripts/runUniversal.sh
+++ work/scripts/runUniversal.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-exe=../compiled/Universal
+exe=imsrg++
+#exe=../compiled/Universal
 #exe="valgrind --track-origins=yes ../compiled/Universal"
 
 #vnn=/itch/exch/BlockGen/me2j/chi2b_srg0800_eMax12_lMax10_hwHO020.me2j.gz
 #v3n=/itch/exch/me3j/chi2b3b400cD-02cE0098_srg0800ho40C_eMax14_EMax14_hwHO020.me3j.gz
 
-vnn=../../input/chi2b_srg0800_eMax12_lMax10_hwHO020.me2j.gz
+vnn=input/chi2b_srg0625_eMax08_hwHO024.me2j.gz
+
+#vnn=../../input/chi2b_srg0800_eMax12_lMax10_hwHO020.me2j.gz
 #v3n=../../input/chi2b3b400cD-02cE0098_srg0800ho40C_eMax12_EMax12_hwHO020.me3j.gz
 v3n=none
 
@@ -23,7 +26,9 @@ v3n=none
 #for ((A=14;A<=28;A++)); do
 for ((A=18;A<=18;A++)); do
 
-hw=20
+hw=24
+reference=O18
+#valence_space=C8
 #valence_space=O$A
 valence_space=sd-shell
 #valence_space=Ca40
@@ -32,10 +37,11 @@ valence_space=sd-shell
 #reference=default
 #reference=Si34
 #reference=Si28
-reference=O$A
+#reference=O$A
 #A=18
-smax=200
-emax=4
+#smax=200
+smax=5
+emax=2
 e3max=12
 #method=HF
 #method=MP3
@@ -45,15 +51,15 @@ method=magnus
 omega_norm_max=0.25
 #flowfile=output/BCH_SingleRef_${}_e${emax}.dat
 file3='file3e1max=12 file3e2max=28 file3e3max=12'
+Operators=
 #Operators=Rp2,Rm2,HCM,HCM_28
 #Operators=Rp2,rho0.0,rho0.2,rho0.5,rho1.0,rho1.2,rho1.5,rho2.0,rho2.5,rho3.0,rho3.5,rho4.5,rho6.0
-#Operators=E2
+Operators=E2
 #Operators=protonFBC1,protonFBC2,protonFBC3,protonFBC4,protonFBC5,protonFBC6,protonFBC7,protonFBC8,protonFBC9
-Operators=
 scratch=SCRATCH
 #scratch=
 
-$exe 2bme=${vnn} 3bme=${v3n} emax=${emax} e3max=${e3max} method=${method} valence_space=${valence_space} hw=${hw} smax=${smax} ${file3} omega_norm_max=${omega_norm_max} reference=${reference} Operators=${Operators} scratch=${scratch} A=${A} use_brueckner_bch=true
+$exe 2bme=${vnn} 3bme=${v3n} emax=${emax} e3max=${e3max} method=${method} valence_space=${valence_space} hw=${hw} smax=${smax} ${file3} omega_norm_max=${omega_norm_max} reference=${reference} Operators=${Operators} scratch=${scratch} A=${A} use_brueckner_bch=false
 
 done
 
