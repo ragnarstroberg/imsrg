bb2a40e1103ff869475f6e3b94971de6af8a23c8
ragnar-MacBookAir <ragnarstroberg@gmail.com>
2017-03-09 17:09:21 -0800

    Some changes made on laptop. Don't quite recall what they are.

--- src/ReadWrite.hh
+++ src/ReadWrite.hh
@@ -37,6 +37,7 @@ class ReadWrite
    void WriteNuShellX_int( Operator& op, string filename);
    void WriteNuShellX_op( Operator& op, string filename);
    void ReadNuShellX_int( Operator& op, string filename);
+   void ReadNuShellX_sp( ModelSpace& ms, string filename);
    void WriteNuShellX_intfile( Operator& op, string filename, string mode); 
    void WriteAntoine_int( Operator& op, string filename); 
    void WriteAntoine_input( Operator& op, string filename, int A, int Z); 
