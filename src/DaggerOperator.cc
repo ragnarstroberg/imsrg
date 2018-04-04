4dd9c34a88cdfa902464d297038ad7a3ecfb7bf5
Ragnar Stroberg <ragnarstroberg@gmail.com>
2018-04-03 17:38:46 -0700

    Created empty DaggerOperator Class files

---
+++ src/DaggerOperator.cc
@@ -0,0 +1,6 @@
+
+#include "DaggerOperator.hh"
+
+
+
+
---
+++ src/DaggerOperator.hh
@@ -0,0 +1,24 @@
+#ifndef DaggerOperator_h
+#define DaggerOperator_h 1
+
+
+#include "Operator.hh"
+
+
+class DaggerOperator: public Operator
+{
+ public:
+  
+  // commutator terms, these are just placeholders for now
+//  void comm110ss( const Operator& X, const Operator& Y) ; 
+//  void comm220ss( const Operator& X, const Operator& Y) ;
+//  void comm111ss( const Operator& X, const Operator& Y) ;
+//  void comm121ss( const Operator& X, const Operator& Y) ;
+//  void comm221ss( const Operator& X, const Operator& Y) ;
+//  void comm122ss( const Operator& X, const Operator& Y) ;
+//  void comm222_pp_hhss( const Operator& X, const Operator& Y) ;
+//  void comm222_phss( const Operator& X, const Operator& Y) ;
+//  void comm222_pp_hh_221ss( const Operator& X, const Operator& Y) ;
+};
+
+#endif 
