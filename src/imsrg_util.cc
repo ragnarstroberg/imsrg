8ec1a35df62f371af2348bfcd29860d2382db1ed
Ragnar Stroberg <stroberg@uw.edu>
2019-07-19 13:46:37 -0700

    Fixed missing return in imsrg_util.cc. Thanks Johannes.

--- src/imsrg_util.cc
+++ src/imsrg_util.cc
@@ -112,7 +112,7 @@ namespace imsrg_util
       {
         double rr;
         istringstream(opnamesplit[1]) >> rr;
-        NeutronDensityAtR(modelspace,rr);
+        return NeutronDensityAtR(modelspace,rr); // Thanks Johannes
       }
       else if (opnamesplit[0] == "OneOcc") // Get occupation of specified orbit, e.g. OneOccp_1p3
       {
