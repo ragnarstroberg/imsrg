20f61bb9680726f2d71efb696df70df7cc032fdd
Ragnar Stroberg <stroberg@uw.edu>
2019-11-26 16:32:09 -0800

    Added OneBodyDensity operator:

--- src/imsrg_util.hh
+++ src/imsrg_util.hh
@@ -64,6 +64,7 @@ namespace imsrg_util
 
  Operator NumberOp(ModelSpace& modelspace, int n, int l, int j2, int tz2);
  Operator NumberOpAlln(ModelSpace& modelspace, int l, int j2, int tz2);
+ Operator  OneBodyDensity(ModelSpace& modelspace,index_t i,index_t j);
  Operator PSquaredOp(ModelSpace& modelspace);
  Operator RSquaredOp(ModelSpace& modelspace);
  Operator E0Op(ModelSpace& modelspace);
