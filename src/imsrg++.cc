af3e4edad35d933cef5cff920f263d8130bf296a
ragnar-MacBookAir <ragnarstroberg@gmail.com>
2021-02-23 17:46:59 -0800

    Added ability to write Tokyo format in FCI mode

--- src/imsrg++.cc
+++ src/imsrg++.cc
@@ -806,34 +806,43 @@ int main(int argc, char** argv)
   if (method == "FCI")
   {
   std::cout << __func__ << "  line " << __LINE__ << std::endl;
-   // we want the 1b piece to be diagonal in the vacuum NO representation
-    HNO = HNO.UndoNormalOrdering();
-    double previous_zero_body = HNO.ZeroBody;
-    modelspace.SetReference("vacuum");
-    HartreeFock hfvac(HNO);
-    hfvac.Solve();
-
-//    Operator Hvac = hfvac.GetNormalOrderedH();
-    HNO = hfvac.GetNormalOrderedH();
-    std::cout << "HNO had zero body = " << HNO.ZeroBody << "  and I add " << previous_zero_body << " to it. " << std::endl;
-    HNO.ZeroBody += previous_zero_body;
-
-    rw.WriteNuShellX_int(HNO,intfile+".int");
-    rw.WriteNuShellX_sps(HNO,intfile+".sp");
-
-    std::cout << "NO wrt vacuum. One Body term is hopfully still diagonal?" << std::endl << HNO.OneBody << std::endl;
-
-    for (index_t i=0;i<ops.size();++i)
-    {
-      ops[i] = ops[i].UndoNormalOrdering();
-      if ((ops[i].GetJRank()+ops[i].GetTRank()+ops[i].GetParity())<1)
+   if ( valence_file_format == "tokyo" )
+   {
+      HNO = HNO.UndoNormalOrdering();
+      rw.WriteTokyo(HNO,intfile+".snt", "");
+      // Haven't yet implemented FCI operators for Tokyo format. I should do this...
+   }
+   else // Write in NuShellX Format
+   {
+     // we want the 1b piece to be diagonal in the vacuum NO representation
+      HNO = HNO.UndoNormalOrdering();
+      double previous_zero_body = HNO.ZeroBody;
+      modelspace.SetReference("vacuum");
+      HartreeFock hfvac(HNO);
+      hfvac.Solve();
+  
+  //    Operator Hvac = hfvac.GetNormalOrderedH();
+      HNO = hfvac.GetNormalOrderedH();
+      std::cout << "HNO had zero body = " << HNO.ZeroBody << "  and I add " << previous_zero_body << " to it. " << std::endl;
+      HNO.ZeroBody += previous_zero_body;
+  
+      rw.WriteNuShellX_int(HNO,intfile+".int");
+      rw.WriteNuShellX_sps(HNO,intfile+".sp");
+  
+      std::cout << "NO wrt vacuum. One Body term is hopfully still diagonal?" << std::endl << HNO.OneBody << std::endl;
+  
+      for (index_t i=0;i<ops.size();++i)
       {
-        rw.WriteNuShellX_op(ops[i],intfile+opnames[i]+".int");
-      }
-      else
-      {
-        rw.WriteTensorOneBody(intfile+opnames[i]+"_1b.op",ops[i],opnames[i]);
-        rw.WriteTensorTwoBody(intfile+opnames[i]+"_2b.op",ops[i],opnames[i]);
+        ops[i] = ops[i].UndoNormalOrdering();
+        if ((ops[i].GetJRank()+ops[i].GetTRank()+ops[i].GetParity())<1)
+        {
+          rw.WriteNuShellX_op(ops[i],intfile+opnames[i]+".int");
+        }
+        else
+        {
+          rw.WriteTensorOneBody(intfile+opnames[i]+"_1b.op",ops[i],opnames[i]);
+          rw.WriteTensorTwoBody(intfile+opnames[i]+"_2b.op",ops[i],opnames[i]);
+        }
       }
     }
     HNO.PrintTimes();
