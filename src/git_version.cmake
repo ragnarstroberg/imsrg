e9451cc3e0ccfd3eef5e8b04242b571885b31581
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-07-13 16:02:19 -0500

    Finish CMakeLists impl to be feature equivalent to makefile
    This includes python bindings, build time build version string generation, and imsrg++ binary.

---
+++ src/git_version.cmake
@@ -0,0 +1,48 @@
+# This is adapted from https://www.mattkeeter.com/blog/2018-01-06-versioning/.
+execute_process(COMMAND git log --pretty=format:'%h' -n 1
+                OUTPUT_VARIABLE GIT_REV
+                ERROR_QUIET)
+
+# Check whether we got any revision (which isn't
+# always the case, e.g. when someone downloaded a zip
+# file from Github instead of a checkout)
+if ("${GIT_REV}" STREQUAL "")
+    set(GIT_REV "N/A")
+    set(GIT_DIFF "")
+    set(GIT_TAG "N/A")
+    set(GIT_BRANCH "N/A")
+else()
+    execute_process(
+        COMMAND bash -c "git diff --quiet --exit-code || echo +"
+        OUTPUT_VARIABLE GIT_DIFF)
+    execute_process(
+        COMMAND git describe --exact-match --tags
+        OUTPUT_VARIABLE GIT_TAG ERROR_QUIET)
+    execute_process(
+        COMMAND git rev-parse --abbrev-ref HEAD
+        OUTPUT_VARIABLE GIT_BRANCH)
+
+    string(STRIP "${GIT_REV}" GIT_REV)
+    string(SUBSTRING "${GIT_REV}" 1 7 GIT_REV)
+    string(STRIP "${GIT_DIFF}" GIT_DIFF)
+    string(STRIP "${GIT_TAG}" GIT_TAG)
+    string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
+endif()
+
+set(VERSION "#include \"version.hh\"
+
+#include <string>
+
+std::string BuildVersion() {
+    return \"${GIT_BRANCH}_${GIT_REV}${GIT_DIFF}\";
+}")
+
+if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/version.cc)
+    file(READ ${CMAKE_CURRENT_BINARY_DIR}/version.cc VERSION_)
+else()
+    set(VERSION_ "")
+endif()
+
+if (NOT "${VERSION}" STREQUAL "${VERSION_}")
+    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/version.cc "${VERSION}")
+endif()
