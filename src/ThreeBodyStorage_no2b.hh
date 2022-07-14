7419dca26b1e487170bc3a5229f94339c45c0d51
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-07-14 14:39:20 -0500

    Remove x86 intrinsic from ThreeBodyStorage_no2b
    This is probably not needed, especially not for half because half will turn on x86 optimizations at compile time if they are supported.

--- src/ThreeBodyStorage_no2b.hh
+++ src/ThreeBodyStorage_no2b.hh
@@ -16,9 +16,6 @@
 #include <boost/iostreams/filter/gzip.hpp>
 
 /// Use half-precision floats. For documentation, see http://half.sourceforge.net
- #ifndef NO_x86
-// #include <x86intrin.h>
- #endif
  #include "half.hpp"
  typedef half_float::half ME_half_type;
  typedef float ME_single_type;
