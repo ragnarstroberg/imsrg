079943e15773fea7bea6cc2bedbd69bdf00d54d6
Ragnar Stroberg <rstroberg@nd.edu>
2023-02-23 09:30:04 -0500

    Updated pybind11 because python3.11 changed the API, breaking backwards compatibility.

--- src/pybind11/tests/test_async.py
+++ src/pybind11/tests/test_async.py
@@ -1,9 +1,10 @@
-import asyncio
 import pytest
-from pybind11_tests import async_module as m
+
+asyncio = pytest.importorskip("asyncio")
+m = pytest.importorskip("pybind11_tests.async_module")
 
 
-@pytest.fixture
+@pytest.fixture()
 def event_loop():
     loop = asyncio.new_event_loop()
     yield loop
@@ -15,7 +16,7 @@ async def get_await_result(x):
 
 
 def test_await(event_loop):
-    assert 5 == event_loop.run_until_complete(get_await_result(m.SupportsAsync()))
+    assert event_loop.run_until_complete(get_await_result(m.SupportsAsync())) == 5
 
 
 def test_await_missing(event_loop):
