8954ad96c98b0d5e8e7ce1d0973c0b067bae085f
Ragnar Stroberg <sstrobe2@nd.edu>
2023-05-11 23:21:57 -0400

    Playing with how to handle scalar operators that change Z or parity, e.g. should they be reduced?

--- src/imsrg++.cc
+++ src/imsrg++.cc
@@ -805,6 +805,10 @@ int main(int argc, char** argv)
 //  for (auto& op : ops)
    for (size_t i=0;i<ops.size();++i)
    {
+     if ( ops[i].GetJRank()==0 and(  ops[i].GetTRank()!=0 or ops[i].GetParity()!=0 ) )
+     {
+       ops[i].MakeNotReduced(); // We can treat these as scalars, even though imsrg_util makes them reduced.
+     }
 //     std::cout << "Before transforming  " << opnames[i] << " has 3b norm " << ops[i].ThreeBodyNorm() << std::endl;
       // We don't transform a DaggerHF, because we want the a^dagger to already refer to the HF basis.
      if ((basis == "HF") and (opnames[i].find("DaggerHF") == std::string::npos)  )
@@ -1419,6 +1423,11 @@ int main(int argc, char** argv)
       }
 //      Operator op = imsrg_util::OperatorFromString( modelspace, opname );
 
+      if ( op.GetJRank()==0 and ( op.GetTRank()!=0 or op.GetParity()!=0 ) )
+      {
+         std::cout << "Before doing HF, making " << opname << "  not reduced" << std::endl;
+         op.MakeNotReduced();
+      }
 
       // Added by Antoine Belley
       if (write_HO_ops)
@@ -1518,6 +1527,10 @@ int main(int argc, char** argv)
        std::cout << "writing tensor files " << std::endl;
       if (valence_file_format == "tokyo")
       {
+        if ( op.GetJRank()==0)
+        {
+          op.MakeReduced(); // This is sneaky and might not be expected behavior, but I'm just testing this now.
+        }
         rw.WriteTensorTokyo(intfile+opname+"_2b.snt",op);
       }
       else
