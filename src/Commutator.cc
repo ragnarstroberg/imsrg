7b327508521eae73bc9c56693c9c4bbefb62c4ab
Ragnar Stroberg <sstrobe2@crcfe02.crc.nd.edu>
2026-07-28 13:29:59 -0400

    Modified comm220ss so that it's not insanely slow while flowing. Also it now passes the unit test for parity changing operators

--- src/Commutator.cc
+++ src/Commutator.cc
@@ -691,6 +691,8 @@ namespace Commutator
     double comm = 0;
     auto &X2 = X.TwoBody;
     auto &Y2 = Y.TwoBody;
+    int hX = X.IsHermitian() ? +1 : -1;
+    int hY = Y.IsHermitian() ? +1 : -1;
     int pX = X.GetParity();
     int pY = Y.GetParity();
     int pZ = (pX + pY) % 2; // Added to make z.parity correct when calling only UnitTest and not through the CommutatorScalarScalar
@@ -701,27 +703,6 @@ namespace Commutator
       Z.profiler.timer[__func__] += omp_get_wtime() - t_start;
       return;
     }
-
-//    for (int ch = 0; ch < Y.nChannels; ++ch)
-//    {
-//      TwoBodyChannel &tbc = Z.modelspace->GetTwoBodyChannel(ch);
-//      auto hh = tbc.GetKetIndex_hh();
-//      if (hh.size() == 0) continue;
-//      auto ph = tbc.GetKetIndex_ph();
-//      auto pp = tbc.GetKetIndex_pp();
-//      arma::uvec nbar_indices = arma::join_cols(hh, ph);
-//      nbar_indices = arma::join_cols(nbar_indices, pp);
-//      if (hh.size() == 0)
-//        continue;
-//      auto nn = tbc.Ket_occ_hh;
-//      arma::vec nbarnbar = arma::join_cols(tbc.Ket_unocc_hh, tbc.Ket_unocc_ph);
-//      auto &X2 = X.TwoBody.GetMatrix(ch).submat(hh, nbar_indices);
-//      arma::mat Y2 = Y.TwoBody.GetMatrix(ch).submat(nbar_indices, hh);
-//      Y2.head_rows(nbarnbar.size()).each_col() %= nbarnbar;
-//      Z.ZeroBody += 2 * (2 * tbc.J + 1) * arma::sum(arma::diagvec(X2 * Y2) % nn); // This could be made more efficient, but who cares?
-//    }
-
-
     
     std::vector<size_t> ch_bra_list, ch_ket_list;
     auto ch_iter = X.TwoBody.MatEl;
@@ -734,68 +715,66 @@ namespace Commutator
 #pragma omp parallel for schedule(dynamic) reduction(+:z0)
     for (int ich = 0; ich < nch; ++ich)
     {
+
       size_t ch_bra = ch_bra_list[ich];
       size_t ch_ket = ch_ket_list[ich];
-      // std::cout << ch_bra << " " << ch_ket << std::endl;
+
       TwoBodyChannel &tbc_bra = X.modelspace->GetTwoBodyChannel(ch_bra);
       TwoBodyChannel &tbc_ket = X.modelspace->GetTwoBodyChannel(ch_ket);
-      int J = tbc_bra.J;
       int nbras = tbc_bra.GetNumberKets();
       int nkets = tbc_ket.GetNumberKets();
+      int J = tbc_bra.J;
 
-      for (int ibra = 0; ibra < nbras; ibra++)
+      // First <bra|X|ket><ket|Y|bra>, with bra=hh and ket = pp (or the equivalent with occupation factors)
+      auto hh = tbc_bra.GetKetIndex_hh();
+
+      auto nn = tbc_bra.Ket_occ_hh;
+      auto hh_ket = tbc_ket.GetKetIndex_hh();
+      auto ph_ket = tbc_ket.GetKetIndex_ph();
+      arma::vec nbarnbar = arma::ones(tbc_ket.GetNumberKets() );
+      for (size_t i=0;i<hh_ket.size(); i++)
       {
-        Ket &bra = tbc_bra.GetKet(ibra);
-        Orbit &oa = X.modelspace->GetOrbit(bra.p);
-        Orbit &ob = X.modelspace->GetOrbit(bra.q);
-        // std::cout<< oa.n << oa.l << oa.j2 << oa.tz2 << oa.occ << std::endl;
-        // std::cout<< ob.n << ob.l << ob.j2 << ob.tz2 << ob.occ<< std::endl;
-        size_t a = bra.p;
-        size_t b = bra.q;
-        double na = bra.op->occ;
-        double nb = bra.oq->occ;
-        double ab_symm = 2;
-        if (a == b)
-          ab_symm = 1;
-        int ketmin = 0;
-        if (ch_bra == ch_ket)
-          ketmin = ibra;
-        for (int iket = ketmin; iket < nkets; iket++)
-        {
-          Ket &ket = tbc_ket.GetKet(iket);
-          size_t c = ket.p;
-          size_t d = ket.q;
-          Orbit &oc = X.modelspace->GetOrbit(ket.p);
-          Orbit &od = X.modelspace->GetOrbit(ket.q);
-          double nc = ket.op->occ;
-          double nd = ket.oq->occ;
-          double occfactor = na * nb * (1 - nc) * (1 - nd);
-          double cd_symm = 2;
-          if (c == d)
-            cd_symm = 1;
-          // std::cout<< a<<" "<<b<<" "<<c<<" "<<d<<std::endl;
-          double xabcd = X2.GetTBME_J(J, J, bra.p, bra.q, ket.p, ket.q);
-          double yabcd = Y2.GetTBME_J(J, J, bra.p, bra.q, ket.p, ket.q);
-          double xcdab = X2.GetTBME_J(J, J, ket.p, ket.q, bra.p, bra.q);
-          double ycdab = Y2.GetTBME_J(J, J, ket.p, ket.q, bra.p, bra.q);
-          comm = (xabcd * ycdab - yabcd * xcdab);
-          double term = 0;
-          term += 1. / 4 * (2 * J + 1) * ab_symm * cd_symm * occfactor * comm;
-          if (pX == 1 and pY == 1)
-          {
-            term /= 2;
-            comm = xcdab*yabcd - ycdab * xabcd;
-            term += 1. / 4 * (2 * J + 1) * ab_symm * cd_symm * nc*nd*(1-na)*(1-nb) * comm;
-          }
-          z0 += term;
-        }
+         nbarnbar[hh_ket[i]] = tbc_ket.Ket_unocc_hh[i];
       }
+      for (size_t i=0;i<ph_ket.size(); i++)
+      {
+         nbarnbar[ph_ket[i]] = tbc_ket.Ket_unocc_ph[i];
+      }
+      auto &X2 = X.TwoBody.GetMatrix(ch_bra,ch_ket).rows(hh);
+
+      arma::mat Y2 = Y.TwoBody.GetMatrix(ch_bra,ch_ket).rows(hh);
+      Y2.each_row() %= nbarnbar.t();
+      z0 += 2 * (2*J + 1) * hY* arma::sum(arma::diagvec(X2 * Y2.t() ) % nn); // This could be made more efficient, but who cares?
+
+      // if not channel diagonal, we need to the same thing with ket=hh and bra==pp
+      if ( ch_bra != ch_ket)
+      {
+        auto hh = tbc_ket.GetKetIndex_hh();
+
+        auto nn = tbc_ket.Ket_occ_hh;
+	auto hh_bra = tbc_bra.GetKetIndex_hh();
+	auto ph_bra = tbc_bra.GetKetIndex_ph();
+        arma::vec nbarnbar = arma::ones(tbc_bra.GetNumberKets() );
+	for (size_t i=0;i<hh_bra.size(); i++)
+        {
+	   nbarnbar[hh_bra[i]] = tbc_bra.Ket_unocc_hh[i];
+	}
+	for (size_t i=0;i<ph_bra.size(); i++)
+	{
+           nbarnbar[ph_bra[i]] = tbc_bra.Ket_unocc_ph[i];
+	}
+        auto& X2 = X.TwoBody.GetMatrix(ch_bra,ch_ket).cols(hh);
+
+        Y2 = Y.TwoBody.GetMatrix(ch_bra,ch_ket).cols(hh);
+        Y2.each_col() %= nbarnbar;
+        z0 += 2 * (2*J + 1) * hX* arma::sum(arma::diagvec(X2.t() * Y2 ) % nn); // This could be made more efficient, but who cares?
+
+      }// if ch_bra != ch_ket
+
     }
     Z.ZeroBody += z0;
     
-    // std::cout<<"Z0="<< Z.ZeroBody <<std::endl;
     Z.profiler.timer[__func__] += omp_get_wtime() - t_start;
-    // std::cout << "End comm220ss " << std::endl;
   }
 
   //*****************************************************************************************
