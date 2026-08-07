025da9ecb1e5449bec56f55800657531bbbf1971
Ragnar Stroberg <rstroberg@nd.edu>
2026-08-07 10:10:20 -0400

    Added Generator::UpdateGeneral to the python bindings

--- src/pyIMSRG.cc
+++ src/pyIMSRG.cc
@@ -533,7 +533,8 @@ PYBIND11_MODULE(pyIMSRG, m)
           .def("SetOnly1bEta", [](IMSRGSolverPV &self, bool tf)
                { self.GetGeneratorPV().SetOnly1bEta(tf); })
           .def("Transform", [](IMSRGSolverPV &self, Operator &op, Operator &opPV)
-               { return self.Transform(op, opPV); });
+               { return self.Transform(op, opPV); })
+      ;
 
       py::class_<Generator>(m, "Generator")
           .def(py::init<>())
@@ -541,8 +542,10 @@ PYBIND11_MODULE(pyIMSRG, m)
           .def("SetDenominatorPartitioning", &Generator::SetDenominatorPartitioning, py::arg("Moller_Plessett or Epstein_Nesbet"))
           .def("SetUseIsospinAveraging", &Generator::SetUseIsospinAveraging, py::arg("tf"))
           .def("Update", &Generator::Update, py::arg("H"), py::arg("Eta"))
+          .def("UpdateGeneral", &Generator::Update, py::arg("H"), py::arg("H_denom"), py::arg("Eta"))
           .def("GetHod_SingleRef", &Generator::GetHod_SingleRef, py::arg("H"))
-          .def("GetHod", &Generator::GetHod, py::arg("H"));
+          .def("GetHod", &Generator::GetHod, py::arg("H"))
+      ;
 
       py::class_<GeneratorPV, Generator>(m, "GeneratorPV")
           .def(py::init<>())
